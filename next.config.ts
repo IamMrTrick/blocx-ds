d5d02935f5ee218fd4d551afeff6c4e7c87b2bd7
IamMrTrick <mister.3ck@gmail.com>
2025-09-01 00:20:31 +0400

    Update-landing-changes

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: 'standalone',
     // experimental: {
     //     allowedDevOrigins: ["http://192.168.10.14:3000"],
     // },
